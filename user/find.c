60b58362b090fdcc49ccbfd5e145c97aab0dcc7c
charming <4646968841@qq.com>
2022-11-24 17:12:07 +0000

    pass find

---
+++ user/find.c
@@ -0,0 +1,103 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+char*
+fmtname(char *path)
+{
+  // static char buf[DIRSIZ+1];
+  char *p;
+
+  // Find first character after last slash.
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+  // printf("path is:%s \n", path);
+  // printf("fmt1 is:%s \n", p);
+  // Return blank-padded name.
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  // memmove(buf, p, strlen(p));
+  // memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  // printf("fmt is:%s, len is %d \n", p, strlen(p));
+  return p;
+}
+
+void
+find(char *path, char *file_name)
+{
+  // printf("path %s\n", path);
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if((fd = open(path, 0)) < 0){
+    fprintf(2, "find: cannot open %s\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    fprintf(2, "find: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  switch(st.type){
+  case T_DEVICE:
+  case T_FILE:
+    // printf("%s is file name, len is %d \n", file_name, strlen(file_name));
+    if (strcmp(fmtname(path), file_name) == 0)
+      {
+        printf("%s\n", path);
+      }
+    break;
+
+  case T_DIR:
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      printf("ls: path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+
+      if(de.inum == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      // printf("de.name %s\n", de.name);
+      p[DIRSIZ] = 0;
+      if(stat(buf, &st) < 0){
+        printf("ls: cannot stat %s\n", buf);
+        continue;
+      }
+      // printf("buf %s\n", buf);
+      if (strcmp(de.name, ".") != 0 && strcmp(de.name, "..") != 0)
+      {
+        /* code */
+        find(buf, file_name);
+      }
+      
+    //   if (strcmp(fmtname(buf), file_name) != 0)
+    //   {
+    //     printf("%s\n", buf);
+    //   }
+      
+    }
+    break;
+  }
+  close(fd);
+}
+
+int
+main(int argc, char *argv[])
+{
+  if(argc < 3 || argc > 3){
+    printf("find dict filename......");
+    exit(0);
+  }
+  find(argv[1], argv[2]);
+  exit(0);
+}
